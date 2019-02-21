53a5f15beab919821453f6b003e00c07daa89441
Sanjith PK <sanjithpk06@gmail.com>
2019-02-21 11:06:58 +0530

    removing files

--- TEST/material-components-android-codelabs-101-starter/kotlin/shrine/app/src/main/java/com/google/codelabs/mdc/kotlin/shrine/ProductCardViewHolder.kt
+++
@@ -1,7 +0,0 @@
-package com.google.codelabs.mdc.kotlin.shrine
-
-import android.support.v7.widget.RecyclerView
-import android.view.View
-
-class ProductCardViewHolder(itemView: View) //TODO: Find and store views from itemView
-    : RecyclerView.ViewHolder(itemView)
